31d1f4fc7d9ed41db98c18a555b417239b1c0b04
Duyzhii <duyzhii@gmail.com>
2024-12-06 15:35:00 +0700

    feat: add skeleton nft card in homepage

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,8 +5,7 @@ import Introduction from "@/pages/home/components/Introduction";
 import CollectionCarousel from "@/pages/home/components/CollectionCarousel";
 import { useQuery } from "react-query";
 import CreatorCarousel from "./components/CreatorCarousel";
-import SkeletonNftCard from "@/components/skeleton/SkeletonNft";
-import LoadingAnimation from "@/components/ui/loading";
+import { SkeletonNftCarousel } from "@/components/skeleton/SkeletonNft";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const trendingApiEndpoint =
@@ -34,7 +33,34 @@ function Home() {
   } = useQuery("top-creators", () => fetcher(creatorsApiEndpoint));
 
   if (trendingLoading || collectionsLoading || creatorsLoading)
-    return LoadingAnimation();
+    return (
+      <>
+        <div className="flex flex-col items-center justify-center">
+          <Introduction />
+        </div>
+        <div className="flex flex-col gap-32 items-center justify-center">
+          <div className="flex flex-col gap-10 items-center">
+            <span className="text-5xl font-bold leading-normal text-gradient">
+              Trending NFTs
+            </span>
+            <SkeletonNftCarousel />
+          </div>
+          <div className="flex flex-col justify-center items-center gap-10">
+            <span className="text-5xl leading-normal font-bold text-gradient">
+              Top Collections
+            </span>
+            {/* Skeleton for collections here */}
+          </div>
+          <div className="flex flex-col justify-center items-center gap-10">
+            <span className="text-5xl leading-normal font-bold text-gradient">
+              Top Creators
+            </span>
+            {/* Skeleton for creators here */}
+          </div>
+          <Banner />
+        </div>
+      </>
+    );
 
   if (trendingError) return <div>Error: {trendingError.message}</div>;
   if (collectionsError) return <div>Error: {collectionsError.message}</div>;
@@ -50,7 +76,7 @@ function Home() {
           <span className="text-5xl font-bold leading-normal text-gradient">
             Trending NFTs
           </span>
-          <NftCarousel data={trendingData} isLoading={trendingLoading} />
+          <NftCarousel data={trendingData} />
         </div>
         <div className="flex flex-col justify-center items-center gap-10">
           <span className="text-5xl leading-normal font-bold text-gradient">
@@ -62,7 +88,7 @@ function Home() {
           <span className="text-5xl leading-normal font-bold text-gradient">
             Top Creators
           </span>
-          <CreatorCarousel data={creatorsData} isLoading={trendingLoading} />
+          <CreatorCarousel data={creatorsData} />
         </div>
         <Banner />
       </div>
